aa2970542572a505a0b255900572a66521a52519
marketingmba <83208401+marketingmba@users.noreply.github.com>
2021-04-29 15:25:17 +0300

    Delete styleheight.js

--- marketingmba-ROI-main/ROI calculator_files/styleheight.js
+++
@@ -1,8 +0,0 @@
-
-    // Selecting the iframe element
-    var iframe = document.getElementById("Iframe");
-    
-    // Adjusting the iframe height onload event
-    iframe.onload = function(){
-        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-    }
