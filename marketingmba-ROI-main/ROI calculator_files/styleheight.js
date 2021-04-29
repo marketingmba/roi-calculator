81677522912c43eb2a75e9006a8e8af962f9c8bf
marketingmba <83208401+marketingmba@users.noreply.github.com>
2021-04-29 15:14:27 +0300

    Create styleheight.js

---
+++ marketingmba-ROI-main/ROI calculator_files/styleheight.js
@@ -0,0 +1,8 @@
+
+    // Selecting the iframe element
+    var iframe = document.getElementById("Iframe");
+    
+    // Adjusting the iframe height onload event
+    iframe.onload = function(){
+        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+    }
